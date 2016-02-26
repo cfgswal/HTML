2ee755a3c177c7d25b4d6de5dd6223228304415b
cfgswal <Ramon@MacBokk-Pro-de-Ramon.local>
2016-02-26 21:37:58 +0100

    WalRock

---
+++ miweb/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi primera pagina web</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">
+</head>
+
+<body>
+<div style="text-align:center"><img width="200"  src="Imagenes/logo%20rock.png" >
+</div>
+<div style="text-align: center">
+    <h1>WalRock</h1>
+    <p>Conocido como uno de los creadores de tendencias más influyentes en Vitoria-Gasteiz</p>
+    <p>Pasión por la música inconfundible, inteligencia de agudos y una silueta distintiva</p>
+    <p>Activa recaudaciones de fondos para devolver a organizaciones benéficas locales</p>
+    <p>Ha hecho su camino a la cima exudando la excelencia en todo lo hace.</p>
+    <br>
+    <h3>EVENTOS REALIZADOS</h3>
+    <p>Concierto The Collection WalRock en Jimmy Jazz</p>
+    <p>Tributos a The Rolling Stones y Bob Dylan</p>
+    <p>Fiesta Rock en Pabellón Besa Arena</p>
+
+    <br>
+    <h3>PROXIMO EVENTO</h3>
+    <p>Hell Dorado, 28 de Marzo, 22 horas</p>
+
+</div>
+<div style="text-align: right" >
+    <img width="300"  src="Imagenes/top_rock.png">
+</div>
+</body>
+</html>
