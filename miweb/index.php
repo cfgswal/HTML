463c300b4bfe03249116a6892f652e2c6a35bc38
cfgswal <cfgswal@gmail.com>
2017-08-28 12:25:45 +0200

    Update and rename miweb/index.php to  miweb/index.php

--- miweb/index.php
+++ miweb/index.php
@@ -7,27 +7,31 @@
 </head>
 
 <body>
-<div style="text-align:center"><img width="200"  src="Imagenes/logo%20rock.png" >
-</div>
-<div style="text-align: center">
-    <h1>WalRock</h1>
-    <p>Conocido como uno de los creadores de tendencias más influyentes en Vitoria-Gasteiz</p>
-    <p>Pasión por la música inconfundible, inteligencia de agudos y una silueta distintiva</p>
-    <p>Activa recaudaciones de fondos para devolver a organizaciones benéficas locales</p>
-    <p>Ha hecho su camino a la cima exudando la excelencia en todo lo hace.</p>
-    <br>
-    <h3>EVENTOS REALIZADOS</h3>
-    <p>Concierto The Collection WalRock en Jimmy Jazz</p>
-    <p>Tributos a The Rolling Stones y Bob Dylan</p>
-    <p>Fiesta Rock en Pabellón Besa Arena</p>
 
-    <br>
-    <h3>PROXIMO EVENTO</h3>
-    <p>Hell Dorado, 28 de Marzo, 22 horas</p>
+<   B   > TABLA DE MULTIPLICAR> </B>
+<  Tabla   bordear = "1"  >
+<    ?php 
 
-</div>
-<div style="text-align: right" >
-    <img width="300"  src="Imagenes/top_rock.png">
-</div>
-</body>
+Para ($ cabecera = 1; $ cabecera <= 10; $ cabecera ++){
+echo ("<th>");
+echo $cabecera;
+echo ("</th>");
+}
+echo ("</tr>");
+for ($x=1;$x<=10;$x++){
+echo ("<tr>");
+echo ("<th>");
+echo $x;
+echo ("</th>");
+for($y=1;$y<=10;$y++){
+$multiplicacion = $x*$y;
+Echo ( "<td>" );
+Echo ( "  $ multiplicacion);multiplicac
+echo ("</td>");
+}
+echo ("</tr>");
+}
+?>
+</ Table>
+</ Body>
 </html>
